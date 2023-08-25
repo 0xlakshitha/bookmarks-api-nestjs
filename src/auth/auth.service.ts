ced812b5851704a5ebe58dab484245a237de5320
0xlakshitha <lxkshitha@gmail.com>
2023-08-25 12:34:17 +0530

    init

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+export class AuthService {
+
+    constructor(
+        private prismaService: PrismaService
+    ) {}
+
+    signup() {
+        return {
+            message: "I am signup"
+        }
+    }
+
+    signin() {
+        return {
+            message: "I am signin"
+        }
+    }
+
+}
